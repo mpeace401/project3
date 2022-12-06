b57a51a46b93fa33d1f4df8b09e47811094a0ae1
blakelunnn <blakelunnn@tamu.edu>
2022-12-06 02:39:53 -0600

    routes for auth

---
+++ Project/routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+    console.log(req.oidc.isAuthenticated());
+    res.render("index", {
+        title: "Welcome to Rev's",
+        isAuthenticated: req.oidc.isAuthenticated(),
+        user: req.oidc.user,
+    });
+});
+
+module.exports = router;
