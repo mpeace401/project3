7ed7cdde16b2b96238606ad630b5be8f8f5c0450
blakelunnn <blakelunnn@tamu.edu>
2022-12-06 13:32:41 -0600

    added auth

--- Project/index.js
+++ Project/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
-var bodyParser = require('body-parser')
+var indexRouter = require("./routes/index.js");
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
+
+const app = express();
+app.set("views", "views");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(auth(config));
+app.use("/", indexRouter);
+var bodyParser = require('body-parser');
 
 var jsonParser = bodyParser.json();
 const { Pool } = require('pg');
-const dotenv = require('dotenv').config();
 
-const app = express();
 const port = 3001;
 
 const pool = new Pool({ 
@@ -26,7 +44,6 @@ process.on('SIGINT', function() {
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.set("view engine", "ejs");
 
 /**
  * Creates a map for the menu items currently displayed
